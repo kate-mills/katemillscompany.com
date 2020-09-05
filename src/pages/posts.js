b7d73e246b11c1b9a78726146ed49a9a6cc5c3b6
Kate Mills <kate.mills1124@gmail.com>
2020-09-05 15:38:59 -0700

    posts page

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -5,11 +5,39 @@ import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
 // ...GatsbyImageSharpFluid
 const PostsPage = ({ data }) => {
+  const {allMdx:{nodes:posts}} = data
   return (
     <Layout>
       <Hero/>
+      <Posts posts={posts} title="All Posts"/>
     </Layout>
   )
 }
 
+export const query = graphql`
+  {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      totalCount
+      nodes {
+        excerpt
+        frontmatter {
+          slug
+          date(formatString: "MMMM DD, YYYYY")
+          author
+          category
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                src
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
 export default PostsPage
